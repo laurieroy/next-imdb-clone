dda4a6874942784c015735043a0b3d5771d6bc4f
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-03 20:03:00 -0800

    Add styling

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,8 @@ import HeaderIcon from "./HeaderIcon";
 
 export default function Header() {
   return (
-    <div>
-      <div>
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -18,6 +18,7 @@ export default function Header() {
         width={100}
         height={100}
         alt="logo"
+        className="cursor-pointer active:brightness-110"
       />
     </div>
   )
