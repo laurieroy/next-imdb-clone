959be3a739967c6b66bea19b3c191b5d18a8c2ce
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-03 19:35:38 -0800

    Install HeroIcon, add image and icons to header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,24 @@
+import Image from "next/image";
+
+import {HomeIcon, InformationCircleIcon, PhoneIcon, UserIcon} from "@heroicons/react/solid"
+
+import HeaderIcon from "./HeaderIcon";
 
 export default function Header() {
   return (
-    <div><h1>Header</h1></div>
+    <div>
+      <div>
+        <HeaderIcon Icon={HomeIcon} title="HOME" />
+        <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
+        <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+        <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+      </div>
+      <Image 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/220px-IMDB_Logo_2016.svg.png"
+        width={100}
+        height={100}
+        alt="logo"
+      />
+    </div>
   )
 }
