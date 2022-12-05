ece64e191bb7a7793469ebee31fddd26371f732e
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-04 19:41:29 -0800

    Add Navbar component, requests.js from TMDB api

---
+++ utils/requests.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+const API_KEY = process.env.API_KEY
+
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/top_rated?api_key=${API_KEY}&language=en-US`
+  }
+}
