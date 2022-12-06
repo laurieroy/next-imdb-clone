9baf5c327768071aca87a55a73861e53c9c395a7
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-06 14:58:03 -0800

    Add styling to card

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ export default function Card({result}) {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
 
   return (
-    <div>
+    <div className="p-3 hover:text-white cursor-pointer active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
       <Image 
         src={BASE_URL + result.backdrop_path} 
         height={100} 
@@ -13,13 +13,13 @@ export default function Card({result}) {
         layout="responsive" 
         alt="movie image"
       />
-      <div>
-        <p className="trucate">
+      <div className="p-2">
+        <p className="trucate text-lg">
           {result.overview}
         </p>
-        <h2>{result.title || result.name}</h2>
-        <p>{result.release_date || result.first_air_date}
-          <ThumbUpIcon className="h-5" />{result.vote_count}
+        <h2 className="text-lg font-bold">{result.title || result.name}</h2>
+        <p className="flex items-center">{result.release_date || result.first_air_date}
+          <ThumbUpIcon className="h-5 ml-3 mr-1" />{result.vote_count}
         </p>
       </div>
     </div>
